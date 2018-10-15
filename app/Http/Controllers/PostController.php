baef021bba8afe6305faa57474659757d97f557f
Mustapha ibnel <mustaphaibnel@gmail.com>
2018-10-15 11:12:26 +0100

    empty files

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,20 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Http\Requests\PostRequest;
-use App\Post;
 use Artisan;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        
-        return 'ok';
-    }
-
     public function store(Request $request)
     {
        
@@ -22,25 +13,4 @@ class PostController extends Controller
         return $request->Name;
     }
 
-    public function show($id)
-    {
-        $post = Post::findOrFail($id);
-
-        return response()->json($post);
-    }
-
-    public function update(PostRequest $request, $id)
-    {
-        $post = Post::findOrFail($id);
-        $post->update($request->all());
-
-        return response()->json($post, 200);
-    }
-
-    public function destroy($id)
-    {
-        Post::destroy($id);
-
-        return response()->json(null, 204);
-    }
 }
